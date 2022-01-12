a95a9efcb80078a64644d1ae610a58a70711a4d7
Mona Lisa <67054711+likra-sketch@users.noreply.github.com>
2022-01-12 17:58:37 +0100

    add preliminary mobile menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  let mobileMenu = document.getElementById("mobile-menu");
+  if (mobileMenu.style.display === "block") {
+    mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "block";
+  }
+} 
